bcc8fe7a4a09fe21aba8e52d2891829c38455d7f
Orane MERTZ <151020208+OraneMertz@users.noreply.github.com>
2025-04-14 16:38:55 +0200

    rename

--- src/main/java/com/eni/eBIDou/EncheresApplication.java
+++ src/main/java/com/eni/eBIDou/EncheresApplication.java
@@ -1,4 +1,4 @@
-package com.eni.encheres;
+package com.eni.eBIDou;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/eni/eBIDou/EncheresApplicationTests.java
+++ src/test/java/com/eni/eBIDou/EncheresApplicationTests.java
@@ -1,4 +1,4 @@
-package com.eni.encheres;
+package com.eni.eBIDou;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
