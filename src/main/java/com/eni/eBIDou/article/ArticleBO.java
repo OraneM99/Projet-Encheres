b158a9fc6d913afb386fb6b30c6c49ae9d410bc9
Tomalambert <tho.trebmal@gmail.com>
2025-04-15 09:12:48 +0200

    add: service

--- src/main/java/com/eni/eBIDou/article/ArticleBO.java
+++ src/main/java/com/eni/eBIDou/article/ArticleBO.java
@@ -18,11 +18,21 @@ public class ArticleBO {
     private long noArticle;
     private String nomArticle;
     private String description;
+
+    public ArticleBO(long noArticle, String nomArticle, String description, LocalDateTime dateDebutEncheres, float miseAPrix) {
+        this.noArticle = noArticle;
+        this.nomArticle = nomArticle;
+        this.description = description;
+        this.dateDebutEncheres = dateDebutEncheres;
+        this.miseAPrix = miseAPrix;
+    }
+
     private LocalDateTime dateDebutEncheres;
     private LocalDateTime dateFinEncheres;
     private float miseAPrix;
     private float prixVente;
     private boolean etatVente;
+
 
     private UtilisateurBO vendeur;
     private UtilisateurBO acheteur;
