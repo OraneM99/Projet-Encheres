9eb21789a9eca36e6873add7145063f8b807a007
RudyGUILLAUME <rudyguillaume.pro@gmail.com>
2025-04-17 16:45:47 +0200

    Mise en place d'un portail admin avec gestion des utilisateurs et des catégories

--- src/main/java/com/eni/eBIDou/utilisateurs/UtilisateurRepository.java
+++ src/main/java/com/eni/eBIDou/utilisateurs/UtilisateurRepository.java
@@ -1,23 +1,14 @@
 package com.eni.eBIDou.utilisateurs;
 
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
-@Profile("jpa")
 public interface UtilisateurRepository extends JpaRepository<UtilisateurBO, Long> {
-    List<UtilisateurBO> findAll();
-    Optional<UtilisateurBO> findById(Long id);
     Optional<UtilisateurBO> findByPseudo(String pseudo);
     Optional<UtilisateurBO> findByEmailOrPseudo(String email, String pseudo);
     boolean existsByPseudo(String pseudo);
     boolean existsByEmail(String email);
-    boolean existsById(Long id);
-    UtilisateurBO save(UtilisateurBO utilisateur);
-    void deleteById(Long id);
 }
-
