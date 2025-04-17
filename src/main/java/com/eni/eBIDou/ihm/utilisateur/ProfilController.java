91086c9f715f1ae7fd5b4967b1730df1ef406c9c
Nowone33 <137183397+Nowone33@users.noreply.github.com>
2025-04-17 14:21:46 +0200

    page profil premiere partie

--- src/main/java/com/eni/eBIDou/ihm/utilisateur/ProfilController.java
+++ src/main/java/com/eni/eBIDou/ihm/utilisateur/ProfilController.java
@@ -5,11 +5,14 @@ import com.eni.eBIDou.utilisateurs.UtilisateurBO;
 import com.eni.eBIDou.utilisateurs.UtilisateurDTO;
 import com.eni.eBIDou.utilisateurs.UtilisateurMapper;
 import com.eni.eBIDou.utilisateurs.UtilisateurService;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @Controller
 public class ProfilController {
@@ -23,12 +26,39 @@ public class ProfilController {
     }
 
     @GetMapping("/profil")
-    public String afficherProfil(Model model, @AuthenticationPrincipal UtilisateurBO utilisateurConnecte) {
+    public String afficherProfil(Model model, @RequestParam(value="edit", required = false) Boolean editMode, @AuthenticationPrincipal UtilisateurBO utilisateurConnecte) {
 
+        //recuperer l'utilisateur Connecte
         model.addAttribute("utilisateurDTO", utilisateurMapper.toDto(utilisateurConnecte));
 
+        //pour gérer l'action d'appui sur le bouton modifier
+        model.addAttribute("editMode", Boolean.TRUE.equals(editMode));
         return "profil";
     }
+
+    @PostMapping("/profil")
+    public String modifierProfil(@ModelAttribute UtilisateurDTO utilisateurDTO, @AuthenticationPrincipal UtilisateurBO utilisateurConnecte) {
+        //récupère l'id utilisateur du profil connecte
+        long idUtilisateurAmodifier = utilisateurConnecte.getNoUtilisateur();
+
+        //envoie la modification avec l'id du profilCo' et le form utilisateurDTO avec les nouvelles valeurs
+        utilisateurService.update(idUtilisateurAmodifier, utilisateurDTO);
+
+        return "redirect:/profil";
+
+    }
+
+    @PostMapping("/profil/{id}")
+    public String supprimerProfil(@PathVariable long id, @AuthenticationPrincipal UtilisateurBO utilisateurConnecte) {
+        // empêcher de supprimer un autre compte que le sien
+        if (utilisateurConnecte.getNoUtilisateur() != id) {
+            throw new AccessDeniedException("Action non autorisée");
+        }
+        utilisateurService.delete(id);
+        // Redirection vers une page d'accueil
+        return "redirect:/accueil";
+    }
+
 
 
 
