6538e9c7d771f4cfdc9dfd02f9d6c88f599fa0d3
Nowone33 <137183397+Nowone33@users.noreply.github.com>
2025-04-22 12:09:15 +0200

    début enchere
    requete INSERT de création d'enchere.
    sans update.

--- src/main/java/com/eni/eBIDou/ihm/article/DetailVenteController.java
+++ src/main/java/com/eni/eBIDou/ihm/article/DetailVenteController.java
@@ -9,11 +9,14 @@ import com.eni.eBIDou.retrait.RetraitService;
 import com.eni.eBIDou.service.ServiceResponse;
 import com.eni.eBIDou.utilisateurs.UtilisateurBO;
 import com.eni.eBIDou.utilisateurs.UtilisateurDTO;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DetailVenteController {
@@ -43,13 +46,15 @@ public class DetailVenteController {
         Retrait retraitData = retrait.getData();
 
         //Récupérer l'enchère l'enchère liée à l'article
-        ServiceResponse<Enchere> enchere = enchereService.getByArticleCible(articleAVendre);
+        ServiceResponse<Enchere> enchere = enchereService.trouverMeilleureEnchere(articleAVendre.getNoArticle());
         Enchere enchereData = enchere.getData();
 
         UtilisateurBO encherisseur = null;
         if(enchereData != null ) {
             encherisseur = enchereData.getEncherisseur();
         }
+
+
 
 
         model.addAttribute("article", articleAVendre);
@@ -59,7 +64,46 @@ public class DetailVenteController {
         model.addAttribute("encherisseur", encherisseur);
 
 
+
         return "detail-vente";
     }
+
+    @PostMapping("/encherir/{id}")
+    public String encherir(@PathVariable long id, @RequestParam int montant_enchere, @AuthenticationPrincipal UtilisateurBO utilisateurConnecte, Model model) {
+
+
+
+
+        //nécessité de récupérer l'utilisateur connecte qui veut enchérir
+        model.addAttribute("utilisateurConnecte", utilisateurConnecte);
+
+        // Récuperer l'article visité
+        ServiceResponse<Article> article = articleService.getArticleById(id);
+
+        System.out.println("UtilisateurConnecte: "+ utilisateurConnecte);
+        System.out.println("Article: "+ article.getData());
+        System.out.println("Montant de l'enchere" + montant_enchere);
+
+
+        // Vérifier que le montant de l'enchere est plus haut que le montant de départ de l'article
+        if(montant_enchere > article.getData().getMiseAPrix()){
+
+            // vérifier que l'utilisateur a bien un crédit suffisant pour couvrir le montant
+            if(utilisateurConnecte.getCredit() >= montant_enchere) {
+                // récupérer l'id de l'article
+                long idArticle = article.getData().getNoArticle();
+                // récupérer l'id User
+                long idEncherisseur = utilisateurConnecte.getNoUtilisateur();
+
+                ServiceResponse<Enchere> newEnchere = enchereService.placerEnchere(idArticle, idEncherisseur, montant_enchere);
+
+            }
+
+        }
+
+
+        return"redirect:/accueil/";
+    }
+
 
 }
